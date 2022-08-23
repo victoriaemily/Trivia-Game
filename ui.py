aa10feba86cc152486e0ac061e0623e8463e5cf2
vakiko <victoria.c6140@gmail.com>
2022-08-23 11:20:24 -0500

    Initial commit for Quiz Game

---
+++ ui.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+THEME_COLOR = "#375362"
+
+class QuizInterface:
+    
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        
+        
+        self.window.mainloop()
+
